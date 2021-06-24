f8f14f3dd7f570ca1d052dbca7e6eb78d5df171b
supriya-gdptl <supriya.pandhre9@gmail.com>
2021-06-23 23:37:23 -0700

    code visualize the segmented parts

--- visualization/visualize_parts.py
+++ visualization/visualize_parts.py
@@ -1,11 +1,15 @@
 # coding: utf-8
+import h5py
+import numpy as np
+import open3d as o3d
+import copy
+import os
+from colormap import colormap
+
 
 
 def visualize_trimesh_pointcloud():
-    import h5py
     import trimesh
-    import numpy as np
-
 
     a=h5py.File("Chair-01/train-04.h5", mode='r')
     all_mask = a['gt_mask'][...]
@@ -39,76 +43,102 @@ def visualize_trimesh_pointcloud():
     part_pcd.show()
 
 
-def save_open3d_images_of_pointcloud():
+def save_image(point_cloud, output_filename):
     """
-    save renderings of an object with bounding box, coordinate axes
-    It saves the images from three different views
-    :param mesh_path: mesh or point cloud path
-    :param output_image_name: name of the rendered image to save to
+
+    :param point_cloud:
+    :param output_filename:
     :return:
     """
+    # viewing direction
+    dirs = (np.pi/6, np.pi/6, 0)
 
-    import h5py
-    import numpy as np
-    import open3d as o3d
-    import copy
+    # create transformation matrix
+    T = np.eye(4)
+    T[:3, :3] = point_cloud.get_rotation_matrix_from_xyz(dirs)
+    pcd_t = copy.deepcopy(point_cloud).transform(T)
+
+    vis = o3d.visualization.Visualizer()
+    vis.create_window(visible=False, width=256, height=256, left=0, top=0)
+    vis.add_geometry(pcd_t)
+
+    vis.get_render_option().load_from_json('renderoption.json')
+    vis.update_geometry(pcd_t)
+    vis.poll_events()
+    vis.update_renderer()
+    vis.capture_screen_image(f"{output_filename}")
+    vis.destroy_window()
+
+
+def save_open3d_images_of_pointcloud(category, level, point_cloud_idx, output_folder):
+    """
+    save renderings of an object and predicted parts
+    :param category: (string) shape category
+    :param level: (int) level of part segmentation granularity (level=1: coarser, level=3: finer)
+    :param point_cloud_idx: (int) index of shape to save renderings of
+    :param output_folder: (string) folder location to save output renderings
+    :return:
+    """
+    # create output folder
+    os.makedirs(output_folder, exist_ok=True)
+    
+    # get list of colors
+    color_list = colormap()
+    print("color_list.shape: ",color_list.shape)
 
     o3d.utility.set_verbosity_level(o3d.utility.VerbosityLevel.Warning)
-    points_path = "../data/partnet/ins_seg_h5_gt/Bag-1/test-00.h5"
-    predicted_mask_path = "../pretrained_results/Bag/Level_2/test-00.h5"
-    # import os
-    # print(os.path.abspath(points_path))
-    # print(os.path.abspath(predicted_mask_path))
+    # keeping the level to '1' for points path, becoz we care only about the points and all categories have at least level-1 folder
+    points_path = f"../data/partnet/ins_seg_h5_gt/{category}-1/test-00.h5"
     pts = h5py.File(points_path, mode='r')['pts'][...]
+
+    # point cloud index
+    pts0 = pts[point_cloud_idx]
+    # save original point cloud for reference
+    # pcd = o3d.geometry.PointCloud()
+    # pcd.points = o3d.utility.Vector3dVector(pts0)
+    # pcd.paint_uniform_color(np.array([[0.0],[0.0],[0.0]]))
+    # save_image(point_cloud=pcd, output_filename=os.path.join(output_folder, f"img_{category}_L{level}_pcd{point_cloud_idx}.png"))
+
+    # open predicted part segmentation
+    predicted_mask_path = f"../pretrained_results/{category}/Level_{level}/test-00.h5"
     # mask size=(num_examples, 200, 10k)
     mask = h5py.File(predicted_mask_path, mode='r')['mask'][...]
     # valid size = (num_examples, 200). Boolean array
     valid = h5py.File(predicted_mask_path, mode='r')['valid'][...]
-
-    # point cloud index
-    pcd_idx = 0
-
-    pcd_mask = mask[pcd_idx]
+    
+    # get predicted all masks of current point cloud
+    pcd_mask = mask[point_cloud_idx]
     # get valid masks out of 200. ('mask' contains 200 parts, but only some of them represent parts, others are empty)
     # array/channel ids of such non-empty parts is given in 'valid'
-    pcd_valid_part_ids = np.where(valid[pcd_idx]==True)[0]
+    pcd_valid_part_ids = np.where(valid[point_cloud_idx]==True)[0]
+    # Colors array of size (part_ids.shape, 3,1)
+    pcd_color_list = color_list[:pcd_valid_part_ids.shape[0]]
 
-    # save images
-    dirs = (np.pi/8, -np.pi/3, -np.pi/8)
+    # collect part point cloud and colors
+    part_pcd = np.zeros((10000, 3))
+    part_color = np.zeros((10000, 3))
 
     for part_idx in pcd_valid_part_ids:
-        pts0 = pts[pcd_idx]
-
         mask0 = np.expand_dims(pcd_mask[part_idx], axis=1)
 
         # get part coordinates
         part = np.multiply(pts0, mask0)
+        part_pcd += part
 
-        # convert numpy array to open3d point cloud
-        pcd = o3d.geometry.PointCloud()
-        pcd.points = o3d.utility.Vector3dVector(part)
+        # color
+        color = np.tile(pcd_color_list[part_idx],(part.shape[0],1))
+        color = np.multiply(color, mask0)
+        part_color += color
 
-        # create transformation matrix
-        T = np.eye(4)
-        T[:3, :3] = pcd.get_rotation_matrix_from_xyz(dirs)
-        pcd_t = copy.deepcopy(pcd).transform(T)
+    # convert numpy array to open3d point cloud
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(part_pcd)
+    # use "paint_uniform_color(self: open3d.geometry.PointCloud, color: numpy.ndarray[float64[3, 1]]) → None" to assign colors to current part-pointcloud
+    pcd.colors = o3d.utility.Vector3dVector(part_color)
 
-        # box = pcd_t.get_oriented_bounding_box()
-        # box.color = (1, 0, 0)
-
-        vis = o3d.visualization.Visualizer()
-        vis.create_window(visible=False, width=256, height=256, left=0, top=0)
-        vis.add_geometry(pcd_t)
-        # vis.add_geometry(box)
-
-        vis.get_render_option().load_from_json('renderoption.json')
-        vis.update_geometry(pcd_t)
-        vis.poll_events()
-        vis.update_renderer()
-        vis.capture_screen_image(f"bag_level2_img_{pcd_idx}_part_{part_idx}.png")
-        vis.destroy_window()
+    save_image(point_cloud=pcd, output_filename=os.path.join(output_folder, f"img_{category}_L{level}_pcd{point_cloud_idx}_segmented.png"))
 
 
 if __name__ == '__main__':
-    save_open3d_images_of_pointcloud()
+    save_open3d_images_of_pointcloud(category="Bag", level=1, point_cloud_idx=0, output_folder="../../../www/partnet/pretrained_prediction")
 
